3be2f39d51b0607b28d18ff95a86ba9d83230666
병수 <vkxqldtn97@naver.com>
2024-09-29 01:34:59 +0900

    Upload Preferences
    Upload Preferences

---
+++ src/main/java/com/toyproject/jobadream/JobadreamApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.jobadream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobadreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobadreamApplication.class, args);
+	}
+
+}
